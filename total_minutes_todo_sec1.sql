c708894d8b2cf706a5e3870edd1c9fea7121afbc
Shannon <86149746+smdinh@users.noreply.github.com>
2024-05-24 15:24:50 -0700

    Update total_minutes_todo_sec1.sql

--- total_minutes_todo_sec1.sql
+++ total_minutes_todo_sec1.sql
@@ -5,14 +5,26 @@ Next, select the SUM of minutes it will take to do all of the items on the TODO 
 **/
 
 -- code provided by Khan Academy
-CREATE TABLE todo_list (id INTEGER PRIMARY KEY, item TEXT, minutes INTEGER);
-INSERT INTO todo_list VALUES (1, "Wash the dishes", 15);
-INSERT INTO todo_list VALUES (2, "Vacuuming", 20);
-INSERT INTO todo_list VALUES (3, "Learn some stuff on KA", 30);
+CREATE TABLE todo_list
+(
+    id INTEGER PRIMARY KEY,
+    item TEXT,
+    minutes INTEGER
+);
+INSERT INTO todo_list
+VALUES
+(1, "Wash the dishes", 15);
+INSERT INTO todo_list
+VALUES
+(2, "Vacuuming", 20);
+INSERT INTO todo_list
+VALUES
+(3, "Learn some stuff on KA", 30);
 
 -- Step 1: Insert another item into the table
 INSERT INTO todo_list
-VALUES (4, "Tidy bedroom", 60);
+VALUES
+(4, "Tidy bedroom", 60);
 
 -- Step 2: Select the SUM of minutes it'll take to complete the items on the TODO list
 SELECT SUM(minutes)
