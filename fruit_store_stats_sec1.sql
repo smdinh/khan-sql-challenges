871447012cd921031a05a1e1141a439fa2bb8118
Shannon <86149746+smdinh@users.noreply.github.com>
2024-05-24 15:24:26 -0700

    Update fruit_store_stats_sec1.sql

--- fruit_store_stats_sec1.sql
+++ fruit_store_stats_sec1.sql
@@ -30,37 +30,80 @@ Jackfruit - $3.99/lb
 */
 
 -- Step 1: Create a table with at least 5 columns
-CREATE TABLE fruit_haven (
-  id INTEGER PRIMARY KEY, -- Unique identifer of fruit. 
-  name TEXT, -- The name of the fruit.
-  origin TEXT, -- The country of origin of where the fruit came from. 
-  price_per_pound NUMERIC, -- The price per 1 pound of the fruit.
-  in_stock INTEGER, -- The quantity of fruits available in the store at the beginning of the week.
-  purchased INTEGER  -- The quantity of fruits purchased during the week.
+CREATE TABLE fruit_haven
+(
+    id INTEGER PRIMARY KEY,  -- Unique identifer of fruit. 
+    name TEXT,               -- The name of the fruit.
+    origin TEXT,             -- The country of origin of where the fruit came from. 
+    price_per_pound NUMERIC, -- The price per 1 pound of the fruit.
+    in_stock INTEGER,        -- The quantity of fruits available in the store at the beginning of the week.
+    purchased INTEGER        -- The quantity of fruits purchased during the week.
 );
 
 -- Step 2: Fill database with values
-INSERT INTO fruit_haven VALUES (1, "Mangosteen", "Thailand", 8.99, 50, 25);
-INSERT INTO fruit_haven VALUES (2, "Korean Pear", "South Korea", 1.49, 100, 75);
-INSERT INTO fruit_haven VALUES (3, "Persimmon", "USA", 2.99, 80, 60);
-INSERT INTO fruit_haven VALUES (4, "Dragon Fruit", "Vietnam", 4.99, 70, 45);
-INSERT INTO fruit_haven VALUES (5, "Lychee", "Mexico", 5.99, 60, 40);
-INSERT INTO fruit_haven VALUES (6, "Longan", "Thailand", 4.49, 70, 50);
-INSERT INTO fruit_haven VALUES (7, "Pomelo", "China", 3.29, 60, 30);
-INSERT INTO fruit_haven VALUES (8, "Mandarin", "USA", 1.99, 120, 90);
-INSERT INTO fruit_haven VALUES (9, "Durian", "Thailand", 9.99, 30, 20);
-INSERT INTO fruit_haven VALUES (10, "Watermelon", "USA", 0.59, 149, 96);
-INSERT INTO fruit_haven VALUES (11, "Cantaloupe", "USA", 0.79, 100, 74);
-INSERT INTO fruit_haven VALUES (12, "Papaya", "Mexico", 1.99, 100, 80);
-INSERT INTO fruit_haven VALUES (13, "Strawberry", "USA", 3.99, 150, 100);
-INSERT INTO fruit_haven VALUES (14, "Cherry", "USA", 4.99, 120, 80);
-INSERT INTO fruit_haven VALUES (15, "Grapes", "USA", 2.49, 180, 130);
-INSERT INTO fruit_haven VALUES (16, "Banana", "Ecuador", 0.59, 250, 200);
-INSERT INTO fruit_haven VALUES (17, "Fuji Apple", "Japan", 1.99, 200, 164);
-INSERT INTO fruit_haven VALUES (18, "Guava", "Mexico", 2.49, 76, 68);
-INSERT INTO fruit_haven VALUES (19, "Mango", "Mexico", 1.99, 119, 93);
-INSERT INTO fruit_haven VALUES (20, "Star Fruit", "Vietnam", 3.49, 69, 49);
-INSERT INTO fruit_haven VALUES (21, "Jackfruit", "Thailand", 3.99, 53, 36);
+INSERT INTO fruit_haven
+VALUES
+(1, "Mangosteen", "Thailand", 8.99, 50, 25);
+INSERT INTO fruit_haven
+VALUES
+(2, "Korean Pear", "South Korea", 1.49, 100, 75);
+INSERT INTO fruit_haven
+VALUES
+(3, "Persimmon", "USA", 2.99, 80, 60);
+INSERT INTO fruit_haven
+VALUES
+(4, "Dragon Fruit", "Vietnam", 4.99, 70, 45);
+INSERT INTO fruit_haven
+VALUES
+(5, "Lychee", "Mexico", 5.99, 60, 40);
+INSERT INTO fruit_haven
+VALUES
+(6, "Longan", "Thailand", 4.49, 70, 50);
+INSERT INTO fruit_haven
+VALUES
+(7, "Pomelo", "China", 3.29, 60, 30);
+INSERT INTO fruit_haven
+VALUES
+(8, "Mandarin", "USA", 1.99, 120, 90);
+INSERT INTO fruit_haven
+VALUES
+(9, "Durian", "Thailand", 9.99, 30, 20);
+INSERT INTO fruit_haven
+VALUES
+(10, "Watermelon", "USA", 0.59, 149, 96);
+INSERT INTO fruit_haven
+VALUES
+(11, "Cantaloupe", "USA", 0.79, 100, 74);
+INSERT INTO fruit_haven
+VALUES
+(12, "Papaya", "Mexico", 1.99, 100, 80);
+INSERT INTO fruit_haven
+VALUES
+(13, "Strawberry", "USA", 3.99, 150, 100);
+INSERT INTO fruit_haven
+VALUES
+(14, "Cherry", "USA", 4.99, 120, 80);
+INSERT INTO fruit_haven
+VALUES
+(15, "Grapes", "USA", 2.49, 180, 130);
+INSERT INTO fruit_haven
+VALUES
+(16, "Banana", "Ecuador", 0.59, 250, 200);
+INSERT INTO fruit_haven
+VALUES
+(17, "Fuji Apple", "Japan", 1.99, 200, 164);
+INSERT INTO fruit_haven
+VALUES
+(18, "Guava", "Mexico", 2.49, 76, 68);
+INSERT INTO fruit_haven
+VALUES
+(19, "Mango", "Mexico", 1.99, 119, 93);
+INSERT INTO fruit_haven
+VALUES
+(20, "Star Fruit", "Vietnam", 3.49, 69, 49);
+INSERT INTO fruit_haven
+VALUES
+(21, "Jackfruit", "Thailand", 3.99, 53, 36);
 
 -- Step 3: Order your items by price
 SELECT *
@@ -72,9 +115,10 @@ ORDER BY price_per_pound;
 Select SUM(in_stock) AS total_fruits_in_stock
 FROM fruit_haven;
 -- Revenue generated
-Select SUM(price_per_pound*purchased) AS total_revenue
+Select SUM(price_per_pound * purchased) AS total_revenue
 FROM fruit_haven;
 -- Total purchases by origin
-Select origin, SUM(purchased) AS total_purchased
+Select origin,
+       SUM(purchased) AS total_purchased
 FROM fruit_haven
 GROUP BY origin;
