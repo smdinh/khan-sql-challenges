c022a1f3649811e631c7cc9627deebb63e99d305
Shannon <86149746+smdinh@users.noreply.github.com>
2024-05-24 15:24:43 -0700

    Update movies_sorted_sec1.sql

--- movies_sorted_sec1.sql
+++ movies_sorted_sec1.sql
@@ -7,13 +7,30 @@ Sort the results so that the earlier movies are listed first. You should have 2 
 **/
 
 -- code provided by Khan Academy
-CREATE TABLE movies (id INTEGER PRIMARY KEY, name TEXT, release_year INTEGER);
-INSERT INTO movies VALUES (1, "Avatar", 2009);
-INSERT INTO movies VALUES (2, "Titanic", 1997);
-INSERT INTO movies VALUES (3, "Star Wars: Episode IV - A New Hope", 1977);
-INSERT INTO movies VALUES (4, "Shrek 2", 2004);
-INSERT INTO movies VALUES (5, "The Lion King", 1994);
-INSERT INTO movies VALUES (6, "Disney's Up", 2009);
+CREATE TABLE movies
+(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    release_year INTEGER
+);
+INSERT INTO movies
+VALUES
+(1, "Avatar", 2009);
+INSERT INTO movies
+VALUES
+(2, "Titanic", 1997);
+INSERT INTO movies
+VALUES
+(3, "Star Wars: Episode IV - A New Hope", 1977);
+INSERT INTO movies
+VALUES
+(4, "Shrek 2", 2004);
+INSERT INTO movies
+VALUES
+(5, "The Lion King", 1994);
+INSERT INTO movies
+VALUES
+(6, "Disney's Up", 2009);
 
 -- Step 1: Select all movies
 SELECT *
