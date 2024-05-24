757fc9987315d39c128483e0cce950d3d3c60ade
Shannon <86149746+smdinh@users.noreply.github.com>
2024-05-23 20:31:01 -0700

    Update books_table_creation_sec1.sql

--- books_table_creation_sec1.sql
+++ books_table_creation_sec1.sql
@@ -11,12 +11,21 @@ All About Love: New Visions by bell hooks - 4.5
 */
 
 -- Step 1: Create an empty table with columns
-CREATE TABLE favorite_books (id INTEGER PRIMARY KEY, name TEXT, author TEXT, rating NUMERIC); -- I added an extra column for author
+CREATE TABLE favorite_books (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  author TEXT,
+  rating NUMERIC -- Added an extra column for author
+); 
 
 -- Step 2: Fill in empty table with values
-INSERT INTO favorite_books VALUES (1, "Happy Place", "Emily Henry", 5);
-INSERT INTO favorite_books VALUES (2, "Skinny Woman in a Straw Hat", "Hao C. Tran", 4.5);
-INSERT INTO favorite_books VALUES (3, "All About Love: New Visions", "bell hooks", 4.5);
+INSERT INTO favorite_books
+VALUES (1, "Happy Place", "Emily Henry", 5);
+INSERT INTO favorite_books
+VALUES (2, "Skinny Woman in a Straw Hat", "Hao C. Tran", 4.5);
+INSERT INTO favorite_books
+VALUES (3, "All About Love: New Visions", "bell hooks", 4.5);
 
 -- Step 3: Check that the database contains the data
-SELECT * FROM favorite_books;
+SELECT *
+FROM favorite_books;
